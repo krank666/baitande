517ea815a0e13866a530275a8e53c65bea8d5afb
jinlan.du <jinlan.du@cyberplus.com>
2020-06-09 17:29:05 +0800

    首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,36 +9,27 @@
 			</view>
 		</view>
 		<view class="buttons">
-			<!-- <view  class="flex solid-bottom padding align-center user">
-				<view class=""><img src="../../static/logo.png"  class="userImg" alt="" ></view>
-				<view class="padding-sm">用户名</view>
-			</view> -->
-			<!-- <view class="shop">
-				<text class="lg text-gray cuIcon-shop"></text>
-				<view>摆摊</view>
-			</view> -->
-			<view class="flex justify-between">
+			<view class="flex justify-between padding-top-sm">
 				<view  class="flex solid-bottom padding-lr align-center user">
-					<view class=""><img src="../../static/logo.png"  class="userImg" alt="" ></view>
-					<view class="padding-sm">用户名</view>
+					<view class=""><img src="../../static/logo.png"  class="Img" alt="" ></view>
+					<view class="padding-sm">张某某</view>
 				</view>
-				<view  class="flex solid-bottom padding-lr align-center " open-type="getUserInfo" @getuserinfo="test"
-			 @tap="showModal" data-target="DialogModal1">
-					<view class=""><text class="lg cuIcon-shop"></text></view>
-					<view class="padding-sm" >去摆摊</view>
+				<view  class="flex solid-bottom padding-lr align-center ">
+					<button class="cu-btn btns" :class="modalName=='DialogModal1'? 'activeBg' :''" open-type="getUserInfo" @getuserinfo="test"
+					 @tap="showModal" data-target="DialogModal1"> <text class="lg cuIcon-shop"></text>摆摊</button>
 				</view>
-			<!-- 	<view class="cu-item">
-					<text class="lg cuIcon-shop"></text>
-					<text class="text-black">去摆摊</text>
-				</view> -->
+			
+			</view>
+			<view class="flex justify-between padding-bottom-sm">
+				<view  class="flex solid-bottom padding-lr align-center user">
+					<view class="Img"><text class="lg cuIcon-locationfill"></text></view>
+					<view class="padding-sm">当前所在朝阳区东大桥..摊位</view>
+				</view>
 			</view>
 			
-			<button class="cu-btn btns" :class="modalName=='DialogModal1'? 'activeBg' :''" open-type="getUserInfo" @getuserinfo="test"
-			 @tap="showModal" data-target="DialogModal1">摆摊</button>
-			<!-- <button class="cu-btn block line-blue margin-tb-sm lg" disabled>无效状态</button> -->
 		</view>
 		<view class="cu-modal" :class="modalName=='DialogModal1'?'show':''">
-			<view class="cu-dialog dialog">
+			<view class="cu-dialog dialog" style="width:85%;">
 				<view class="cu-bar bg-white justify-end" style="height: 100rpx;">
 					<view class="content">摊位信息</view>
 					<view class="action" @tap="hideModal">
@@ -47,38 +38,20 @@
 				</view>
 				<view class="padding-sm">
 					<form action="" class="form">
-						<view class="flex justify-between border-bot">
-							<view class=" text-bold">摊位名称</view>
-							<view class="text-right"><input  placeholder="必填" name="input" v-model="name"></input></view>
+						<view class="flex justify-between padding-tb-sm border-bot">
+							<view class=" text-bold"><text style="color: red;display: inline-block;vertical-align:middle;" class="margin-right-xs">*</text>摊位名称</view>
+							<view class="text-right " ><input  placeholder="必填" name="input" v-model="name"></input></view>
 						</view>
-						<!-- <view class="flex justify-between">
-							<view class="title">摊位描述</view>
-							<view class="text-right"><input placeholder="卖什么的" name="input" v-model="description"></input></view>
-						</view> -->
 						<view class="margin-bottom text-left">
-							<view class="text-bold padding-tb">摊位描述 <tevt class="fr place">限最多80字</tevt></view>
+							<view class="text-bold padding-tb padding-left-sm">摊位描述 <tevt class="fr place">限最多80字</tevt></view>
 							<textarea  class="textarea" v-model="description" />
-							<!-- <view class="text-right margin-top-sm">{{fontNumber}}/200</view> -->
 						</view>
-						<!-- <view class="cu-form-group padding-left-xl">
-							<view class="title">摊位名称</view>
-							<input placeholder="摊位叫什么" name="input" v-model="name"></input>
-						</view>
-						<view class="cu-form-group padding-left-xl">
-							<view class="title">摊位描述</view>
-							<input placeholder="卖什么的" name="input" v-model="description"></input>
-						</view> -->
-						<!-- <view class="cu-form-group">
-							<view class="title">摊位地址</view>
-							<input placeholder="请输入您的摊位地址" name="input" v-model=""></input>
-						</view> -->
 					</form>
 				</view>
 				<view class="cu-bar bg-white justify-end">
 					<view class="action">
 						<button class="cu-btn line-green text-green" @tap="hideModal">取消</button>
 						<button class="cu-btn bg-green margin-left" @tap="submit">出摊</button>
-
 					</view>
 				</view>
 			</view>
@@ -252,14 +225,6 @@
 		height: 100vh;
 		position: relative;
 	}
-
-	/* .buttons {
-		position: fixed;
-		width: 100%;
-		height: auto;
-		bottom: 0;
-		border-top: 1rpx solid lightgrey;
-	} */
 	.buttons {
 		position: fixed;
 		width: 98%;
@@ -277,21 +242,13 @@
 	.box {
 		width: 100%;
 	}
-
-	/* .btns {
-		width: 80%;
-		height: 70rpx;
-		margin: 20rpx 10%;
-		color: #fff;
-		background-color: orange;
-	} */
 	.btns {
-		width: 120upx;
-		height: 80rpx;
-		padding: 10upx;
+		min-width: 120upx;
+		height: 60rpx;
+		padding: 20upx;
 		color: #fff;
 		/* border-radius: 50%; */
-		margin-bottom: 40upx;
+		/* margin-bottom: 40upx; */
 		margin-left: 30upx;
 		background-color: #007fff;
 		box-shadow: 4px 4px 4px 0 rgba(61, 122, 255, .6);
@@ -317,20 +274,20 @@
 
 	.title {
 		font-size: 30rpx;
-		/* color: #666666; */
 	}
 	.content{
 		height: auto;
 	}
-	.justify-between {
+	/* .justify-between {
 		padding: 23upx 0;
-	}
+	} */
+	
 	.border-bot{
 		border-bottom: 1px solid #e2e5ec;
 	}
 	.form{
 		display: block;
-		padding: 20rpx 30rpx 50rpx 30rpx;
+		padding: 30rpx;
 		background: #fff;
 		border-radius: 8rpx;
 	}
@@ -348,18 +305,18 @@
 	.shop{
 		width: 33%;
 	}
-	.userImg{
+	.Img{
 		width: 50upx;
 		height: 50upx;
 	}
 	.cuIcon-shop{
-		color: #007fff!important;
-		font-size: 40rpx;
+		color: #fff!important;
+		font-size: 30rpx;
+		margin-right: 10rpx;
+	}
+	.cuIcon-locationfill{
+		font-size: 50rpx;
+		color: #007fff;
+	}
 
-	}
-	.cu-item:after{
-		content: '';
-		-webkit-transform:scale(0)!important;
-		transform:scale(0)!important;
-	}
 </style>
